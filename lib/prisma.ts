552d239c5fa40bde61a26a27594764adc5803942
Ebenebe Chidera <ebenebechidera3@gmail.com>
2026-04-15 00:48:25 +0100

    add recommended next js singleton connection pattern

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,24 @@
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/prisma/generated/prisma/client';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  // Create the connection pool for Supabase
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  // Wrap it in the Prisma PostgreSQL adapter
+  const adapter = new PrismaPg(pool);
+
+  // Pass the adapter to the PrismaClient constructor
+  return new PrismaClient({ adapter });
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
